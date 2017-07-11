83302eadb51190cdd312f5b3ba29e336142640f1
Cristopher M.R <crian@tigomail.cr>
2017-07-11 00:43:55 -0600

    Signed-off-by: Cristopher M.R <crian@tigomail.cr>

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -14,22 +14,22 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-    <link href="{{asset('plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{asset('app/plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
 
     <!-- Waves Effect Css -->
-    <link href="{{asset('plugins/node-waves/waves.css')}}" rel="stylesheet" />
+    <link href="{{asset('app/plugins/node-waves/waves.css')}}" rel="stylesheet" />
 
     <!-- Animation Css -->
-    <link href="{{asset('plugins/animate-css/animate.css')}}" rel="stylesheet" />
+    <link href="{{asset('app/plugins/animate-css/animate.css')}}" rel="stylesheet" />
 
     <!-- Morris Chart Css-->
-    <link href="{{asset('plugins/morrisjs/morris.css')}}" rel="stylesheet" />
+    <link href="{{asset('app/plugins/morrisjs/morris.css')}}" rel="stylesheet" />
 
     <!-- Custom Css -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('app/css/panel/style.css')}}" rel="stylesheet">
 
     <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-    <link href="{{asset('css/themes/all-themes.css')}}" rel="stylesheet" />
+    <link href="{{asset('app/css/panel/themes/all-themes.css')}}" rel="stylesheet" />
 </head>
 
 <body class="theme-red">
@@ -1105,43 +1105,43 @@
 </section>
 
 <!-- Jquery Core Js -->
-<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('app/plugins/jquery/jquery.min.js')}}"></script>
 
 <!-- Bootstrap Core Js -->
-<script src="{{asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
+<script src="{{asset('app/plugins/bootstrap/js/bootstrap.js')}}"></script>
 
 <!-- Select Plugin Js -->
-<script src="{{asset('plugins/bootstrap-select/js/bootstrap-select.js')}}"></script>
+<script src="{{asset('app/plugins/bootstrap-select/js/bootstrap-select.js')}}"></script>
 
 <!-- Slimscroll Plugin Js -->
-<script src="{{asset('plugins/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
+<script src="{{asset('app/plugins/jquery-slimscroll/jquery.slimscroll.js')}}"></script>
 
 <!-- Waves Effect Plugin Js -->
-<script src="{{asset('plugins/node-waves/waves.js')}}"></script>
+<script src="{{asset('app/plugins/node-waves/waves.js')}}"></script>
 
 <!-- Jquery CountTo Plugin Js -->
-<script src="{{asset('plugins/jquery-countto/jquery.countTo.js')}}"></script>
+<script src="{{asset('app/plugins/jquery-countto/jquery.countTo.js')}}"></script>
 
 <!-- Morris Plugin Js -->
-<script src="{{asset('plugins/raphael/raphael.min.js')}}"></script>
-<script src="{{asset('plugins/morrisjs/morris.js')}}"></script>
+<script src="{{asset('app/plugins/raphael/raphael.min.js')}}"></script>
+<script src="{{asset('app/plugins/morrisjs/morris.js')}}"></script>
 
 <!-- ChartJs -->
-<script src="{{asset('plugins/chartjs/Chart.bundle.js')}}"></script>
+<script src="{{asset('app/plugins/chartjs/Chart.bundle.js')}}"></script>
 
 <!-- Flot Charts Plugin Js -->
-<script src="plugins/flot-charts/jquery.flot.js"></script>
-<script src="plugins/flot-charts/jquery.flot.resize.js"></script>
-<script src="plugins/flot-charts/jquery.flot.pie.js"></script>
-<script src="plugins/flot-charts/jquery.flot.categories.js"></script>
-<script src="plugins/flot-charts/jquery.flot.time.js"></script>
+<script src="{{asset('app/plugins/flot-charts/jquery.flot.js')}}"></script>
+<script src="{{asset('app/plugins/flot-charts/jquery.flot.resize.js')}}"></script>
+<script src="{{asset('app/plugins/flot-charts/jquery.flot.pie.js')}}"></script>
+<script src="{{asset('app/plugins/flot-charts/jquery.flot.categories.js')}}"></script>
+<script src="{{asset('app/plugins/flot-charts/jquery.flot.time.js')}}"></script>
 
 <!-- Sparkline Chart Plugin Js -->
-<script src="plugins/jquery-sparkline/jquery.sparkline.js"></script>
+<script src="{{asset('app/plugins/jquery-sparkline/jquery.sparkline.js')}}"></script>
 
 <!-- Custom Js -->
-<script src="js/admin.js"></script>
-<script src="js/pages/index.js"></script>
+<script src="{{asset('app/js/admin.js')}}"></script>
+<script src="{{asset('app/js/pages/index.js')}}"></script>
 
 
 </body>
